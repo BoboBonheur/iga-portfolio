a23d61bdc59e02c0a9508def55ade08226ed53ed
Bobo <bizibyose2020@gmail.com>
2023-10-16 00:01:04 +0400

    portfolio iga

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    document.getElementById("pdfButton").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.open("./Bobocv2023.pdf", "_blank"); // Replace "your-pdf-file.pdf" with the actual PDF file path
+    });
